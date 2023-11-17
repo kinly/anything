c374c444c362fc4d460c04a154e531020803da88
kinly <kinly.lei@gmail.com>
2023-11-17 19:14:32 +0800

    Update property_simple.h

--- property_simple.h
+++ property_simple.h
@@ -6,16 +6,8 @@
 
 namespace property {
     class manager;
-};
 
-class creature {
-private:
-    property::manager _property_mgr;
-
-public:
-    property::manager& get_property_mgr() {
-        return _property_mgr;
-    }
+    class creature;
 };
 
 namespace property {
@@ -65,7 +57,7 @@ namespace property {
     /// @brief value
     class value final : public meta {
     public:
-        using setted_function = std::function(void(creature*, value*, def::value_type));
+        using setted_function = std::function<void(creature*, value*, def::value_type)>;
 
     private:
         def::key_type _key = 0;
@@ -74,14 +66,7 @@ namespace property {
         setted_function _setted = nullptr;
 
     private:
-        void __refresh(creature* owner) {
-            auto old_value = _value;
-            _value = meta::calculate();
-
-            if (_setted) {
-                _setted(owner, this, old_value);
-            }
-        }
+        void __refresh(creature* owner);
 
     public:
         explicit value(def::key_type key, setted_function&& setted)
@@ -125,92 +110,9 @@ namespace property {
         std::unordered_set<def::key_type> _last_changed;
 
     private:
-        static value::setted_function __setted_function(def::key_type key) {
+        static value::setted_function __setted_function(def::key_type key);
 
-            auto DEFAULT_SET = [](creature* owner, value* v, def::value_type old_value) {
-                if (owner == nullptr) return;
-
-                static constexpr def::value_type min_value = 0;
-                static constexpr def::value_type max_value = 10000;
-
-                static constexpr def::value_type anti_force = 1;
-
-                auto& mgr = owner->get_property_mgr();
-
-                bool anti_monitor = false;
-
-                do {
-                    auto calculate_value = v->get_value(true);
-                    if (calculate_value < min_value || calculate_value > max_value) {
-                        if (anti_monitor) {
-                            v->meta::set_percent(0);
-                            mgr.set_basic(v->get_key(), anti_force);
-                            break;
-                        }
-                        else {
-                            v->meta::set_basic(calculate_value < min_value ? min_value : max_value);
-                        }
-                    }
-
-                    if (anti_monitor) {
-                        if (v->get_value() != v->get_value(true)) {
-                            v->set_basic(owner, v->meta::get_basic());
-                        }
-                        break;
-                    }
-                    anti_monitor = true;
-                } while (true);
-
-                mgr.record_changed(v->get_key());
-            };
-
-            auto MAXHP_SET = [DEFAULT_SET](creature* owner, value* v, def::value_type old_value) {
-                if (owner == nullptr) return;
-                auto& mgr = owner->get_property_mgr();
-                const auto current_hp = mgr.get(static_cast<def::key_type>(key::hp));
-                if (current_hp > v->get_value() && mgr._borned) {
-                    mgr.set_basic(static_cast<def::key_type>(key::hp), v->get_value());
-                } else if (current_hp < v->get_value() && mgr._borned) {
-                    mgr.add_basic(static_cast<def::key_type>(key::hp), v->get_value() - old_value);
-                }
-                DEFAULT_SET(owner, v, old_value);
-            };
-
-            auto HP_SET = [DEFAULT_SET](creature* owner, value* v, def::value_type old_value) {
-                if (owner == nullptr) return;
-                auto& mgr = owner->get_property_mgr();
-                const auto max_hp = mgr.get(static_cast<def::key_type>(key::max_hp));
-                if (v->get_value() > max_hp) {
-                    v->meta::set_percent(0);
-                    mgr.set_basic(v->get_key(), max_hp);
-                }
-                DEFAULT_SET(owner, v, old_value);
-            };
-
-            auto MAXHPPERCENT_SET = [DEFAULT_SET](creature* owner, value* v, def::value_type old_value) {
-                if (owner == nullptr) return;
-                auto& mgr = owner->get_property_mgr();
-                mgr.set_percent(static_cast<def::key_type>(key::max_hp), v->get_value());
-                DEFAULT_SET(owner, v, old_value);
-            };
-
-            static std::unordered_map<def::key_type, value::setted_function> setted_functions = {
-                { key::max_hp, MAXHP_SET },
-                { key::hp, HP_SET },
-                { key::max_hp_percent, MAXHPPERCENT_SET },
-            };
-
-            auto iter = setted_functions.find(key);
-            return iter == setted_functions.end() ? DEFAULT_SET : iter->second;
-        }
-
-        auto __key_iterator(def::key_type key, bool inexist_emplace = false) -> decltype(_propertys)::iterator{
-            auto iter = _propertys.find(key);
-            if (iter == _propertys.end() && inexist_emplace) {
-                return _propertys.emplace(key, value(key, __setted_function(key))).first;
-            }
-            return iter;
-        }
+        auto __key_iterator(def::key_type key, bool inexist_emplace = false) -> decltype(_propertys)::iterator;
 
         void record_changed(def::key_type key) {
             _last_changed.emplace(key);
@@ -223,6 +125,10 @@ namespace property {
         }
 
         ~manager() = default;
+
+        void set_borned() {
+            _borned = true;
+        }
 
         def::value_type get(def::key_type key) const {
             const auto iter = _propertys.find(key);
@@ -302,5 +208,15 @@ namespace property {
             }
             return false;
         }
+    };
+
+    class creature {
+    private:
+        property::manager _property_mgr;
+
+    public:
+        creature();
+
+        property::manager& get_property_mgr();
     };
 };
